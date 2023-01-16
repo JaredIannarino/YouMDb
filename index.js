24b38184dec0ab20342bd575a414d07453efe4e5
JaredIannarino <Iannarinoj@gmail.com>
2023-01-16 15:13:38 -0500

    commit #6
    bug fixes

--- index.js
+++ index.js
@@ -1,79 +1,54 @@
 let searchInput = document.getElementById("search-input")
 const preSearch = document.getElementById("pre-search")
 const postSearch = document.getElementById('post-search')
-let html1 = ``
-let html2 = []
-let urlArray = []
-let filmsData = ''
+const form = document.getElementById("search-form")
 
-document.addEventListener("click", function(event) {
+let searchInputValue = ''
+
+form.addEventListener("submit", async function(event) {
+    searchInputValue = searchInput.value.replace(' ', '+')
     event.preventDefault()
-  event.stopPropagation()
-  if (event.target.matches(".fa-magnifying-glass") || event.target.matches(".search-btn-2")) {
-      let searchInputValue = searchInput.value
-      preSearch.style.display = "none"
-      html1 = ``;
-        fetch(`https://www.omdbapi.com/?apikey=31a874b3&s=${searchInputValue}`)
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data)
-        getFilmsId(data.Search)
-        })
-  }
+    // Fetch a list of movies/data
+    // turn off pre-search modal
+    const res = await fetch(`https://www.omdbapi.com/?apikey=31a874b3&s=${searchInputValue}`)
+    const data = await res.json()
+        if (data.Response === 'True') {
+            getMovieFromApi(data.Search)
+            preSearch.style.display = "none"
+            } else{
+                console.log("No Matching Films")
+            }
+   
 })
 
-function getFilmsId(filmsArray){
-    urlsArray = []
-    let titlesArray = []
-    filmsArray.forEach(function(films){
-        urlsArray.push(films.Poster)
-        titlesArray.push(films.Title)
+function getMovieFromApi(moviesArray){
+    let listsHtml = ``
+    moviesArray.forEach(function(movie){
+            fetch(`https://www.omdbapi.com/?apikey=31a874b3&i=${movie.imdbID}`)
+        .then(res => res.json())
+        .then(data => {
+            listsHtml += `
+                <div class="film-wpr">
+                    <div id="poster-wpr" class="film-poster-wpr">
+                        <img src="${data.Poster}">
+                    </div>
+                    <div class="title-rating-wpr">
+                        <h1 class="film-title">${data.Title}</h1>
+                        <p1 class="film-rating">8/10</p1>
+                    </div>
+                    <p1 class="film-runtime">${data.Runtime}</p1>
+                    <p1 class="film-genre">${data.Genre}</p1>
+                    <button class="add-to-watchlist">Watchlist</button>
+                    <p1 class="plot">${data.Plot}</p1>
+                </div>
+    `   
+           render(listsHtml) 
+        })
+        
     })
-    getFilmsData(titlesArray, urlsArray)
-    // console.log(urlsArray)
-}
-
-function getFilmsData(array1, array2){
-    let filmData = ""
-    array1.forEach(async function(title){
-        const res = await fetch(`https://www.omdbapi.com/?t=${title}&&apikey=88275857`)
-    const data = await res.json()
-    .then(data => {
-        filmsData += data
-        htmlIfy2(html2, data)
-        })     
-    }) 
-    array2.forEach(function(url){
-        let imgHtml = `<img src="${url}" class="poster">`
-        html2.push(imgHtml)
-    })
-
     
 }
 
-function htmlIfy2(array, filmData){
-    array.forEach(function(urls){
-        html1 += `
-    <div class="film-wpr">
-        <div id="poster-wpr" class="film-poster-wpr">
-        ${urls}
-        </div>
-        <div class="title-rating-wpr">
-            <h1 class="film-title">${filmData.Title}</h1>
-            <p1 class="film-rating">8/10</p1>
-        </div>
-        <p1 class="film-runtime">${filmData.Runtime}</p1>
-        <p1 class="film-genre">${filmData.Genre}</p1>
-        <button class="add-to-watchlist">Watchlist</button>
-        <p1 class="plot">${filmData.Plot}</p1>
-    </div>
-    `    
-    })
-     render(html1)   
-}
-
-
-
-function render(htmlStrings){
-    postSearch.innerHTML = `${htmlStrings}`
+function render(html){
+    postSearch.innerHTML = `${html}`
 }
