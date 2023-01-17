6433f5c0e7d09250e17446d8b2d17838322f3db9
JaredIannarino <Iannarinoj@gmail.com>
2023-01-17 17:02:05 -0500

    css fixes

--- hof.js
+++ hof.js
@@ -41,7 +41,7 @@ function renderHof() {
                     </div>
                     <p1 class="film-runtime">${data.Runtime}</p1>
                     <p1 class="film-genre">${data.Genre}</p1>
-                    <button class="add-to-hof" data-imdb-id="${data.imdbID}"><i class="fa-solid fa-circle-plus"></i>Hall of Fame</button>
+                    <button class="add-to-hof" data-imdb-id="${data.imdbID}"><i class="fa-solid fa-circle-minus"></i>Hall of Fame</button>
                     <p1 class="plot">${data.Plot}</p1>
                 </div>
                 `);                
