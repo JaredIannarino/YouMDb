c464fdb0866d53eca065b573453bcf159247eada
JaredIannarino <Iannarinoj@gmail.com>
2023-01-17 16:38:23 -0500

    Commit #15
    fixed issue with adding to Media data base

--- hof.js
+++ hof.js
@@ -1,4 +1,8 @@
+const storageKey = 'hofIds';
+
 const hofSection = document.getElementById("hof-section")
+let imdbIds;
+
 
 window.onload = load()
 
@@ -8,8 +12,41 @@ document.addEventListener("click", function(e){
     }
 })
 
-function load(){
-    const hofHtmlStrings = localStorage.getItem("hofHtml")
-    console.log(hofHtmlStrings)
-    hofSection.innerHTML =`${hofHtmlStrings}`
+function load() {
+    let json = localStorage.getItem(storageKey);
+    
+    if (json) {
+        imdbIds = JSON.parse(json)
+    } else {
+        imdbIds = [];
+    }
+    renderHof();
 }
+
+
+function renderHof() {
+
+    hofSection.innerHTML = '';
+    
+    imdbIds.forEach(function(imdbId) {
+        fetch(`https://www.omdbapi.com/?apikey=31a874b3&i=${imdbId}`)
+            .then(res => res.json())
+            .then(data => {
+                hofSection.insertAdjacentHTML('beforeend', `
+                    <div class="film-wpr">
+                        <img class="film-poster" src="${data.Poster}">
+                    <div class="title-rating-wpr">
+                        <h1 class="film-title">${data.Title}</h1>
+                        <p1 class="film-rating"><span><i class="fa-solid fa-star"></i></span>${data.imdbRating}</p1>
+                    </div>
+                    <p1 class="film-runtime">${data.Runtime}</p1>
+                    <p1 class="film-genre">${data.Genre}</p1>
+                    <button class="add-to-hof" data-imdb-id="${data.imdbID}"><i class="fa-solid fa-circle-plus"></i>Hall of Fame</button>
+                    <p1 class="plot">${data.Plot}</p1>
+                </div>
+                `);                
+            })
+        
+    });
+}
+
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 
+const storageKey = 'hofIds'; // I like to keep this in a constant to avoid "magic strings" in code
+
 let searchInput = document.getElementById("search-input")
 const preSearch = document.getElementById("pre-search")
 const postSearch = document.getElementById('post-search')
 const form = document.getElementById("search-form")
 const StartExploring = document.getElementById("start-exploring")
 let searchInputValue = ''
-let hofHtml = ``
+let imdbIDs; // don't initialize the value, this will be done on page load
+
+function init() {
+    // we like to keep a cache of the item that's in localStorage...
+    const json = localStorage.getItem(storageKey);
+    
+    if(json) {
+        imdbIDs = JSON.parse(json)
+    } else {
+        imdbIDs = []; // if there wasn't an entry in local storage create an empty list
+    }
+}
 
 form.addEventListener("submit", async function(event) {
     searchInputValue = searchInput.value.replace(' ', '+')
@@ -29,8 +42,6 @@ form.addEventListener("submit", async function(event) {
 document.addEventListener("click", function(e){
     if(e.target.id === "hof"){
         window.location.href = "hof.html"
-    }else if(e.target.id === "add-to-hof"){
-        
     }
 })
 
@@ -58,36 +69,16 @@ function getMoviesFromApi(moviesArray){
         })
     });
 
-    // Add event listener for "add-to-hof" buttons
     postSearch.addEventListener("click", function(e) {
-        e.preventDefault()
         if (e.target.classList.contains("add-to-hof")) {
-            const imdbID = e.target.getAttribute("data-imdb-id");
-            getMovieFromAPI(imdbID);
-            // Code to send imdbID to hof.html page
+            const imdbId = e.target.dataset.imdbId;
+            
+            if( !imdbIDs.includes(imdbId)) { // only add the id if it doesn't already exist in the list
+                imdbIDs.push(imdbId);
+            }
+            localStorage.setItem(storageKey, JSON.stringify(imdbIDs))
         }
     });
 }
 
-function getMovieFromAPI(id){
-    fetch(`https://www.omdbapi.com/?apikey=31a874b3&i=${id}`)
-        .then(res => res.json())
-        .then(data => {
-            hofHtml += `
-                <div class="film-wpr">
-                        <img class="film-poster" src="${data.Poster}">
-                    <div class="title-rating-wpr">
-                        <h1 class="film-title">${data.Title}</h1>
-                        <p1 class="film-rating"><span><i class="fa-solid fa-star"></i></span>${data.imdbRating}</p1>
-                    </div>
-                    <p1 class="film-runtime">${data.Runtime}</p1>
-                    <p1 class="film-genre">${data.Genre}</p1>
-                    <button class="add-to-hof" data-imdb-id="${data.imdbID}"><i class="fa-solid fa-circle-plus"></i>Hall of Fame</button>
-                    <p1 class="plot">${data.Plot}</p1>
-                </div>
-    `   
-            localStorage.setItem('hofHtml', hofHtml)
-        })
-        
-}
-
+init();
